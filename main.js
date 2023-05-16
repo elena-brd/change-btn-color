25498b366289bb48d7df3229a3f68c15e1ac25b1
elena-brd <128292991+elena-brd@users.noreply.github.com>
2023-05-16 15:30:05 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const changeColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g},${b})`;
+}
+
+const buttons = document.querySelectorAll('button');
+for (let button of buttons) {
+    button.addEventListener('click', colorize);
+}
+
+
+function colorize() {
+    this.style.backgroundColor = changeColor();
+    this.style.color = changeColor();
+    this.style.borderColor = changeColor();
+}
